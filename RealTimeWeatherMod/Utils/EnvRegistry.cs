18e1733e1b4cc1611ed8dc4279ddc1218134437e
Xibuwo <75096547+Xibuwo@users.noreply.github.com>
2025-12-18 18:49:14 +0100

    Update EnvRegistry.cs

--- RealTimeWeatherMod/Utils/EnvRegistry.cs
+++ RealTimeWeatherMod/Utils/EnvRegistry.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using Bulbul;
 
 namespace ChillWithYou.EnvSync.Utils
 {
     internal static class EnvRegistry
     {
-        private static readonly Dictionary<EnvironmentType, EnviromentController> _map = new Dictionary<EnvironmentType, EnviromentController>();
-        public static bool TryGet(EnvironmentType type, out EnviromentController ctrl) => _map.TryGetValue(type, out ctrl);
-        public static void Register(EnvironmentType type, EnviromentController ctrl) { if (ctrl != null) _map[type] = ctrl; }
+        private static readonly Dictionary<EnvironmentType, EnvironmentController> _map = new Dictionary<EnvironmentType, EnvironmentController>();
+        public static bool TryGet(EnvironmentType type, out EnvironmentController ctrl) => _map.TryGetValue(type, out ctrl);
+        public static void Register(EnvironmentType type, EnvironmentController ctrl) { if (ctrl != null) _map[type] = ctrl; }
         public static int Count => _map.Count;
     }
 }
